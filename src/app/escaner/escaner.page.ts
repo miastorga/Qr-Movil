ea2e5a70aabd94b77f20ce1a2da0488db2702fef
Miguel Astorga <mastorga.leiva@gmail.com>
2022-10-24 22:28:51 -0300

    Detalles
    -en el sidemenu recuperar contraseña e historial removidos
    -iconos usuarios con mayot tamaño

--- src/app/escaner/escaner.page.ts
+++ src/app/escaner/escaner.page.ts
@@ -21,42 +21,4 @@ export class EscanerPage implements OnInit {
 				console.log('Error', err)
 			})
 	}
-
-	// async startScanner() {
-	// 	try {
-	// 		const permission = await this.checkPermission()
-	// 		if (!permission) {
-	// 			return
-	// 		}
-	// 		await BarcodeScanner.hideBackground()
-	// 		const result = await BarcodeScanner.startScan()
-	// 		BarcodeScanner.showBackground()
-	// 		if (result?.hasContent) {
-	// 			this.scannedResult = result.content
-	// 			BarcodeScanner.showBackground()
-	// 			document.querySelector('body').classList.remove('scanner-active')
-	// 		}
-	// 	} catch (error) {
-	// 		this.stopScan()
-	// 	}
-	// }
-	// async checkPermission() {
-	// 	try {
-	// 		const status = await BarcodeScanner.checkPermission({ force: true })
-	// 		if (status.granted) {
-	// 			return true
-	// 		}
-	// 		return false
-	// 	} catch (error) {
-	// 		console.log(error)
-	// 	}
-	// }
-	// stopScan() {
-	// 	BarcodeScanner.showBackground()
-	// 	BarcodeScanner.stopScan()
-	// 	document.querySelector('body').classList.remove('scanner-active')
-	// }
-	// ngOnDestroy(): void {
-	// 	this.stopScan()
-	// }
 }
